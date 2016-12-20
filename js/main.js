b17fe5bc53c8e2c60b0a3571538efae450664bd4
Paliy Rostyslav <paliy1984@gmail.com>
2016-12-20 16:49:28 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* Coded by Paliy Rostyslav. e-mail: paliy1984@gmail.com. skype: ros.coprandos  !!!SYLB!!! */
+
+/*jslint vars: true, devel: true*/
+/*global $, jQuery, alert*/
+/*jslint plusplus: true */
+
+
+/*
+$(function () {
+    $('.scroll-down').click(function () {
+        $('html, body').animate({
+            scrollTop: $('section.ok').offset().top
+        }, 'slow');
+        return false;
+    });
+});
+*/
